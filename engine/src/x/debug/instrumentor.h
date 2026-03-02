1773e2e3cd007b42ac5e65c2d13e58a0031a9cc4
dingrui <Bannirui@outlook.com>
2026-03-02 11:05:57 +0800

    宏 跨平台

--- engine/src/x/debug/instrumentor.h
+++ engine/src/x/debug/instrumentor.h
@@ -122,11 +122,24 @@ private:
 
 #define X_PROFILE 1
 
-// cross platform, windows or linux
-#if defined(_MSC_VER)
-#define X_FUNC_SIG __FUNCSIG__
-#else
+// 跨平台兼容
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || \
+    defined(__ghs__)
 #define X_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+#define X_FUNC_SIG __PRETTY_FUNCTION__
+#elif defined(__FUNCSIG__)
+#define X_FUNC_SIG __FUNCSIG__
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+#define X_FUNC_SIG __FUNCTION__
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+#define X_FUNC_SIG __FUNC__
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+#define X_FUNC_SIG __func__
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+#define X_FUNC_SIG __func__
+#else
+#define X_FUNC_SIG "X_FUNC_SIG unknown!"
 #endif
 
 #if X_PROFILE
