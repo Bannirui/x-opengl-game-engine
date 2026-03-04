7adbc576a8dbcbd0f5672b5ba93494a8ee32f6d5
dingrui <Bannirui@outlook.com>
2026-03-04 16:51:45 +0800

    imgui字体

--- engine/src/x/imgui/im_gui_layer.cpp
+++ engine/src/x/imgui/im_gui_layer.cpp
@@ -23,15 +23,18 @@ void ImGuiLayer::OnAttach()
     // 创建上下文
     ImGui::CreateContext();
     // 要启用的功能
-    ImGuiIO &io = ImGui::GetIO();
+    ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // 不仅支持鼠标操作 还支持键盘操作 键盘导航
     io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;  // 支持更改鼠标形状
     io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;   // 可以主动设置鼠标位置
+    // 字体
+    io.Fonts->AddFontFromFileTTF("asset/fonts/opensans/OpenSans-Bold.ttf", 18.0f);
+    io.FontDefault = io.Fonts->AddFontFromFileTTF("asset/fonts/opensans/OpenSans-Regular.ttf", 18.0f);
     // 主题
     ImGui::StyleColorsDark();
-    XApplication &app = XApplication::Get();
+    XApplication& app = XApplication::Get();
     // ImGui绑定glfw 事件自动托管 不用自己手动管理
-    ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow *>(app.get_window().get_nativeWindow()), true);
+    ImGui_ImplGlfw_InitForOpenGL(static_cast<GLFWwindow*>(app.get_window().get_nativeWindow()), true);
     // ImGui绑定OpenGL
     ImGui_ImplOpenGL3_Init(X_GL_VERSION_CORE);
 }
@@ -48,11 +51,11 @@ void ImGuiLayer::OnUpdate(Timestep ts) {}
 
 void ImGuiLayer::OnImguiRender() {}
 
-void ImGuiLayer::OnEvent(Event &e)
+void ImGuiLayer::OnEvent(Event& e)
 {
     if (m_blockEvents)
     {
-        ImGuiIO &io = ImGui::GetIO();
+        ImGuiIO& io = ImGui::GetIO();
         e.Handled |= e.IsInCategory(kEventCategoryMouse) & io.WantCaptureMouse;
         e.Handled |= e.IsInCategory(kEventCategoryKeyboard) & io.WantCaptureKeyboard;
     }
@@ -70,8 +73,8 @@ void ImGuiLayer::Begin()
 void ImGuiLayer::End()
 {
     X_PROFILE_FUNCTION();
-    ImGuiIO      &io  = ImGui::GetIO();
-    XApplication &app = XApplication::Get();
+    ImGuiIO&      io  = ImGui::GetIO();
+    XApplication& app = XApplication::Get();
     io.DisplaySize =
         ImVec2(static_cast<float>(app.get_window().GetWidth()), static_cast<float>(app.get_window().GetHeight()));
     // 渲染
--- engine/src/x/window.h
+++ engine/src/x/window.h
@@ -18,7 +18,7 @@ struct WindowProps
     uint32_t    width;
     uint32_t    height;
 
-    WindowProps(const std::string &title = "X-OpenGL-GAME-ENGINE", uint32_t width = 1280, uint32_t height = 720)
+    WindowProps(const std::string& title = "X-OpenGL-GAME-ENGINE", uint32_t width = 1600, uint32_t height = 900)
         : title(title), width(width), height(height)
     {
     }
@@ -28,7 +28,7 @@ struct WindowProps
 class Window
 {
 public:
-    using EventCallbackFn = std::function<void(Event &)>;
+    using EventCallbackFn = std::function<void(Event&)>;
 
     virtual ~Window() = default;
 
@@ -37,14 +37,14 @@ public:
     virtual uint32_t GetWidth() const  = 0;
     virtual uint32_t GetHeight() const = 0;
 
-    virtual void SetEventCallback(const EventCallbackFn &callback) = 0;
+    virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
     virtual void SetVSync(bool enabled)                            = 0;
     virtual bool IsVSync()                                         = 0;
 
     // 暴露glfw的窗体
-    virtual void *get_nativeWindow() const = 0;
+    virtual void* get_nativeWindow() const = 0;
 
-    static X::Scope<Window> Create(const WindowProps &props = WindowProps());
+    static X::Scope<Window> Create(const WindowProps& props = WindowProps());
 
 protected:
     Window() = default;
