fe76383dfe3b6b91a4fbe49b3a00e343a9a07664
Bannirui <Bannirui@outlook.com>
2026-03-06 23:48:53 +0800

    fix file dialog

--- engine/src/platform/linux/linux_platform_util.cpp
+++ engine/src/platform/linux/linux_platform_util.cpp
@@ -4,43 +4,50 @@
 
 #ifdef __linux__
 
-    #include "pch.h"
     #include "x/util/platform_util.h"
 
     #include <cstdio>
     #include <array>
     #include <memory>
+    #include <optional>
 
 static std::optional<std::string> ExecCommand(const char* cmd)
 {
-    std::array<char, 256> buffer{};
-    std::string           result;
+    std::array<char, 256>                    buffer{};
+    std::string                              result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) { return std::nullopt; }
+    if (!pipe)
+    {
+        return std::nullopt;
+    }
     while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
     {
         result += buffer.data();
     }
-    if (result.empty()) { return std::nullopt; }
+    if (result.empty())
+    {
+        return std::nullopt;
+    }
     // remove trailing newline
-    if (result.back() == '\n') { result.pop_back(); }
+    if (result.back() == '\n')
+    {
+        result.pop_back();
+    }
     return result;
 }
 
 std::optional<std::string> FileDialog::OpenFile(const char* filter)
 {
+    (void)filter;  // avoid unused warning
     std::string command = "zenity --file-selection";
-    auto result = ExecCommand(command.c_str());
-    if (result) { return result; }
-    return std::nullopt;
+    return ExecCommand(command.c_str());
 }
 
 std::optional<std::string> FileDialog::SaveFile(const char* filter)
 {
+    (void)filter;  // avoid unused warning
     std::string command = "zenity --file-selection --save --confirm-overwrite";
-    auto result = ExecCommand(command.c_str());
-    if (result) { return result; }
-    return std::nullopt;
+    return ExecCommand(command.c_str());
 }
 
 #endif
