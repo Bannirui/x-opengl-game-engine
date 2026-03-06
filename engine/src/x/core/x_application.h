d7a34ae3a4f94fdddf49fdcab0f37520eea3e7c8
Bannirui <Bannirui@outlook.com>
2026-03-06 23:51:00 +0800

    fix

--- engine/src/x/core/x_application.h
+++ engine/src/x/core/x_application.h
@@ -14,42 +14,44 @@ class Event;
 class WindowCloseEvent;
 class Layer;
 
-int main(int argc, char **argv);
+int main(int argc, char** argv);
 
 class XApplication
 {
 public:
-    XApplication(const std::string &name = "X APP");
+    XApplication(const std::string& name = "X APP");
     virtual ~XApplication();
 
-    void OnEvent(Event &e);
+    void OnEvent(Event& e);
 
-    void PushLayer(Layer *layer);
-    void PushOverlay(Layer *layer);
+    void PushLayer(Layer* layer);
+    void PushOverlay(Layer* layer);
 
     void Close();
 
-    Window              &get_window() const { return *m_window; }
-    ImGuiLayer          *get_ImGuiLayer() const { return m_ImGuiLayer; }
-    static XApplication &Get() { return *s_instance; }
+    Window& get_window() const { return *m_window; }
+
+    ImGuiLayer* get_ImGuiLayer() const { return m_ImGuiLayer; }
+
+    static XApplication& Get() { return *s_instance; }
 
 private:
     void run();
-    bool onWindowClose(WindowCloseEvent &e);
-    bool onWindowResize(WindowResizeEvent &e);
+    bool onWindowClose(WindowCloseEvent& e);
+    bool onWindowResize(WindowResizeEvent& e);
 
 private:
-    std::unique_ptr<Window> m_window;
-    bool                    m_running{true};
-    LayerStack              m_layerStack;
-    ImGuiLayer             *m_ImGuiLayer;
-    float                   m_lastFrameTime{0.0f};
-    bool                    m_minimized{false};  // 窗口最小化
+    X::Scope<Window> m_window;
+    bool             m_running{true};
+    LayerStack       m_layerStack;
+    ImGuiLayer*      m_ImGuiLayer;
+    float            m_lastFrameTime{0.0f};
+    bool             m_minimized{false};  // 窗口最小化
 
 private:
-    static XApplication *s_instance;
-    friend int           main(int argc, char **argv);
+    static XApplication* s_instance;
+    friend int           main(int argc, char** argv);
 };
 
 // To be defined in the CLIENT
-XApplication *CreateApplication();
+XApplication* CreateApplication();
