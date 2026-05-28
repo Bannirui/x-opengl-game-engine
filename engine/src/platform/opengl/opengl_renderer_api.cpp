b7b8919d410e81c7210b57f88e7536cdb3c1ad25
dingrui <Bannirui@outlook.com>
2026-05-28 18:14:00 +0800

    DrawArrays方式绘制

--- engine/src/platform/opengl/opengl_renderer_api.cpp
+++ engine/src/platform/opengl/opengl_renderer_api.cpp
@@ -42,9 +42,21 @@ void OpenGLRendererAPI::DrawIndexed(const X::Ref<VertexArray>& vertexArray, uint
     glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
 }
 
+/**
+ * 画线段
+ * @param vertexArray VAO 用DrawArrays并用不到VAO
+ * @param vertexCount 用VBO里面多少个顶点绘制
+ */
 void OpenGLRendererAPI::DrawLines(const X::Ref<VertexArray>& vertexArray, uint32_t vertexCount)
 {
     vertexArray->Bind();
+    /**
+     * 真正让GPU开始渲染
+     * - 每两个点组成1个线段
+     * - 从VBO的0号点开始
+     * - 用多少个顶点
+     * 比如vertexCount=2就是画一条线 vertexCount=4就是画两条线
+     */
     glDrawArrays(GL_LINES, 0, vertexCount);
 }
 
