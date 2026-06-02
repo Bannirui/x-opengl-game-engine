8a3d97a2bafbf65c3a1dc2827515d41b40169864
dingrui <Bannirui@outlook.com>
2026-06-02 23:34:53 +0800

    format

--- engine/src/renderer/renderer.cpp
+++ engine/src/renderer/renderer.cpp
@@ -4,17 +4,16 @@
 
 #include "x/renderer/renderer.h"
 
+#include "x/renderer/orthographic_camera.h"
+#include "x/renderer/render_command.h"
 #include "x/renderer/renderer_2D.h"
 #include "x/renderer/renderer_3D.h"
-#include "x/renderer/vertex_array.h"
-#include "x/renderer/render_command.h"
-#include "x/renderer/orthographic_camera.h"
 #include "x/renderer/shader.h"
+#include "x/renderer/vertex_array.h"
 
 X::Scope<Renderer::SceneData> Renderer::s_sceneData = X::CreateScope<Renderer::SceneData>();
 
-void Renderer::Init()
-{
+void Renderer::Init() {
     X_PROFILE_FUNCTION();
     RenderCommand::Init();
     Renderer2D::Init();
@@ -22,26 +21,23 @@ void Renderer::Init()
     // Renderer3D::Init();
 }
 
-void Renderer::Shutdown()
-{
+void Renderer::Shutdown() {
     Renderer3D::Shutdown();
     Renderer2D::Shutdown();
 }
 
-void Renderer::OnWindowResize(uint32_t width, uint32_t height)
-{
+void Renderer::OnWindowResize(uint32_t width, uint32_t height) {
     RenderCommand::SetViewport(0, 0, width, height);
 }
 
-void Renderer::BeginScene(OrthographicCamera &camera)
-{
+void Renderer::BeginScene(OrthographicCamera& camera) {
     s_sceneData->viewProjectionMatrix = camera.get_viewProjectionMatrix();
 }
 
 void Renderer::EndScene() {}
 
-void Renderer::Submit(const X::Ref<Shader> &shader, const X::Ref<VertexArray> &vertexArray, const glm::mat4 &transform)
-{
+void Renderer::Submit(const X::Ref<Shader>& shader, const X::Ref<VertexArray>& vertexArray,
+                      const glm::mat4& transform) {
     shader->Bind();
     shader->SetMat4("u_ViewProjection", s_sceneData->viewProjectionMatrix);
     shader->SetMat4("u_Transform", transform);
